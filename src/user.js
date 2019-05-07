35c3318fe9e3db709c5171481e337e5546a15dc8
mazdak <nazemi.works@gmail.com>
2019-05-07 09:20:15 -0700

    model - schema - user create in user.js template

--- src/user.js
+++ src/user.js
@@ -1,4 +1,13 @@
-//model
+//user model - all of data in single collection
+//collection of users create in db 
 const mongoose = require('mongoose');
-
+//property of mongoose
 const Schema = mongoose.Schema;
+//schema is small part of model
+const UserSchema = new Schema({
+  name: String
+});
+// now creating user model with user collection name - User entire collection
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
