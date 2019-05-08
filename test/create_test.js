168e2b870bb3d950dc99972a3bd834515d8afaff
mazdak <nazemi.works@gmail.com>
2019-05-08 05:16:35 -0700

    create test complete use global promises and mocha before and before each

--- test/create_test.js
+++ test/create_test.js
@@ -4,10 +4,21 @@
 
 //to make assertion need new lib - not include in mocha.
 const assert = require('assert');
+//need to access to user.js to see it work correctly
+const User = require('../src/user');
+
 describe('Creating records', () => {
-  it('save a user', () => {
-    //assertion = hope
-    assert(1 + 1 === 3);
+  it('save a user', (done) => {
+    //assertion = hope - creat user - save it - is saved?
+    const joe = new User({
+      name: 'joe'
+    });
+    joe.save() //when joe still not saved have prop in mongoose flag named isNew
+      .then(() => {
+        // has been joe saved??
+        assert(!joe.isNew);
+        done();
+      })
   });
 });
 //define test command in package.json
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
+// for promise instead of bluebird and q
+mongoose.Promise = global.Promise;
+
 /////////////////WE ARE DOING CRUD
-// we tell to mongoose to connect to mdb through mocha we check it
-mongoose.connect('mongodb://127.0.0.1:27017/users_test', {
-  useNewUrlParser: true
-});
-mongoose.connection //ip:port  / name of db
-  .once('open', () => console.log('good to go'))
-  .on('error', error => {
-    console.warn('Warning', error);
+//executed only one time == to make sure 100% we are connected.
+//signal to mocha wait until i connect 
+before(() => {
+
+  // we tell to mongoose to connect to mdb through mocha we check it
+  mongoose.connect('mongodb://localhost/users_test', {
+    useNewUrlParser: true
   });
+  mongoose.connection //ip:port  / name of db
+    .once('open', () => {})
+    .on('error', error => {
+      console.warn('Warning', error);
+    });
+});
+//************************************* */
+//run hook - is func run before anything executed to clean up db for check 
+//test work other wise test always pass
+beforeEach(done => {
+  mongoose.connection.collections.users.drop(() => {
+    done(); //signal to mocha to run next test.
+  });
+});
