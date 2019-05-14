adde7a739cdcb1561da2c1a5d3164c6d48217150
mazdak <nazemi.works@gmail.com>
2019-05-14 04:00:20 -0700

    skip limit sort in mocha usage as func rq

--- test/reading_test.js
+++ test/reading_test.js
@@ -4,14 +4,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of database', () => {
-  let joe;
+  let joe,maria,zico,zac;
   //db is empty use bE to first make then check it
   beforeEach((done) => {
     //to make sure we can make reference to joe from bot don't use var key
-    joe = new User({
-      name: 'joe'
-    });
-    joe.save() //when joe still not saved have prop in mongoose flag named isNew
+    joe = new User({name: 'joe'});
+    maria = new User({name: 'maria'});
+    alex = new User({name: 'alex'});
+    zac = new User({name: 'zac'});
+    //when joe still not saved have prop in mongoose flag named isNew
+    Promise.all([joe.save(),alex.save(),maria.save(),zac.save()])
       .then(() => done());
   });
   // assert is it guy. belong to it mean.
@@ -37,5 +39,20 @@ describe('Reading users out of database', () => {
         assert(user.name === 'joe');
         done();
       });
+  });
+  xit('can skip and limit the result set', (done) => {
+    //skip first user and limit to 2;
+    User.find({})
+      .sort({ name : 1 })
+      .skip(1)
+      .limit(2)
+      .then((users)=>{
+        //maybe lag of internet cause sequence become different
+        assert(users.length === 2);
+        assert(users[0] === 'joe');
+        assert(users[1] === 'maria');
+
+        done();
+      })
   });
 });
